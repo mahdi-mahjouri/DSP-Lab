f771bb7a10bca7bcba642eaafd29442b79947b71
Mahdi Mahjouri Namin <200044476+mahdi-mahjouri@users.noreply.github.com>
2025-02-20 21:15:13 +0330

    create new MATLAB script.
    new MATLAB script has been added.
    Sections 1-1 up to 1-4 except second part of Section 1-3 have been completed.

---
+++ HW1/HW_1.m
@@ -0,0 +1,92 @@
+%% DSP Lab - Homwork1
+% Mahdi Mahjouri Namin 40023125
+% Nima Nourizadeh 40023***
+
+close all;
+clear;
+clc;
+%% 
+% *Section 1-1*
+
+
+t = 0:0.01:2;
+A = 5;
+f = 1;
+phi = 0;
+signal = A*sin(2*pi*f*t + phi);
+figure('Name', 'Continuous-Time Sinusoidal Wave');
+plot(t, signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinusoidal Wave using plot');
+grid on;
+% ---------------------------------------
+figure('Name', 'Continuous-Time Sinusoidal Wave');
+stem(t, signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Sinusoidal Wave using stem');
+grid on;
+
+
+%% 
+% *Section 1-2*
+
+
+rand_num = rand(1,length(t)) - 0.5;
+rand_signal = rand_num + signal;
+figure('Name', 'Random Signal');
+subplot(211)
+plot(t, signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Original Sinusoidal Wave');
+grid on;
+subplot(212)
+plot(t, rand_signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Noisy Sinusoidal Wave');
+grid on;
+
+%% 
+% *Section 1-3*
+
+M1 = 0;
+M2 = 20;
+window_size = M1 + M2 + 1;
+
+kernel = ones(1, window_size) / window_size;
+filtered_signal = conv(rand_signal,kernel, "same");
+
+figure('Name', 'Moving Average');
+plot(t, filtered_signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Filtered Signal (Moving Average)');
+grid on;
+hold on;
+plot(t, rand_signal, 'LineWidth', 1,'Color','r');
+
+% ???????????????????
+
+%% 
+% *Section 1-4*
+
+M1 = 0;
+M2 = 20;
+window_size = M1 + M2 + 1;
+
+% Define the moving average filter coefficients
+kernel = ones(1, window_size) / window_size;
+
+% Apply the moving average filter using the 'filter' function
+filtered_signal = filter(kernel, 1, rand_signal);
+
+% Plot the results
+figure('Name', 'Moving Average');
+plot(t, filtered_signal, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Filtered Signal (Moving Average)');
+grid on;
